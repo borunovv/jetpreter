5d4ebcc7877176c25a1e033e99c889474c97ec21
borunovv <borunovv@herocraft.com>
2018-08-03 20:39:15 +0200

    javacc R&D

--- src/main/java/com/borunovv/jetpreter/Main.java
+++ src/main/java/com/borunovv/jetpreter/Main.java
@@ -1,8 +1,15 @@
 package com.borunovv.jetpreter;
 
+import com.borunovv.jetpreter.javacc.generated.Program;
+
+import java.io.StringReader;
+
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    private static String PROGRAM = "var a = 5\n";
 
+    public static void main(String[] args) throws Exception {
+        Program parser = new Program(new StringReader("var"));
+        parser.var();
     }
 }
