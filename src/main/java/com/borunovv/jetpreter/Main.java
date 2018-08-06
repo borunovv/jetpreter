ee100fb70a14aa4daed63a1e9dfeaf759082ce38
borunovv <borunovv@herocraft.com>
2018-08-06 18:22:14 +0200

    Added AST node templating

--- src/main/java/com/borunovv/jetpreter/Main.java
+++ src/main/java/com/borunovv/jetpreter/Main.java
@@ -1,15 +1,20 @@
 package com.borunovv.jetpreter;
 
-import com.borunovv.jetpreter.javacc.generated.Program;
+import com.borunovv.jetpreter.javacc.generated.Node;
+import com.borunovv.jetpreter.javacc.generated.ProgramParser;
 
 import java.io.StringReader;
 
 public class Main {
 
-    private static String PROGRAM = "var a = 5\n";
+    private static String PROGRAM = "" +
+            "var a = -5.0\n" +
+            "print \"ABC D\"\n" +
+            "out 123\n";
 
     public static void main(String[] args) throws Exception {
-        Program parser = new Program(new StringReader("var"));
-        parser.var();
+        ProgramParser parser = new ProgramParser(new StringReader(PROGRAM));
+        parser.Program();
+        Node root = parser.rootNode();
     }
 }
